7d9e7cbc75944b2cbf585967d3abe373a8f13c52
Nicolò Mazzucato <nikomazzu@gmail.com>
2013-05-03 13:00:35 +0200

    risolti problemi con le query di test

--- Server/test/com/mytalk/server/data/storage/popolamentoTestCalls.sql
+++ Server/test/com/mytalk/server/data/storage/popolamentoTestCalls.sql
@@ -1,2 +1,2 @@
-INSERT INTO Calls(caller,receiver,duration,startdate,byteSent,byteReceived) VALUES("user0","user1",600,"26-04-2013 13:34:45",10000,10000),
-	("user1","user0",500,"26-04-2012 13:34:45",10000,10000);
+INSERT INTO Calls VALUES(1,"user0","user1",600,'02-02-2013 16:44:56',10000,10000),
+	(2,"user0","user1",600,'02-02-2013 10:53:32',10000,10000);

60e75ab2c74941483999e33fef68cb7a5f9bc1fd
Stefano Campese <stefano.campese.90@gmail.com>
2013-05-09 11:19:57 +0200

    inizio creazione liste

--- Client/MyTalk/web-app/js/router/Router.js
+++ Client/MyTalk/web-app/js/router/Router.js
@@ -1,9 +1,9 @@
 MyTalk.Router.map(function(match) {
   this.route("index", {path:'/'});
-  this.route("lists");
-    /*, function() {
+  this.route("lists");/*, function() {
     this.resource('list', { path: ':list_id' });
   });*/
+  this.route('list', { path: ':list_id' });
 });
 
 MyTalk.IndexRoute = Ember.Route.extend({
@@ -17,6 +17,10 @@ MyTalk.IndexRoute = Ember.Route.extend({
 });
 
 MyTalk.ListsRoute = Ember.Route.extend({
+  model: function() {
+    return MyTalk.WList.find();
+  },
+
   renderTemplate: function(controller, model) {
     this.render('lists');
     this.render('listscontent'); 
@@ -26,3 +30,9 @@ MyTalk.ListsRoute = Ember.Route.extend({
     });*/
   }
 });
+/*
+MyTalk.ListRoute = Ember.Route.extend({
+  model: function() {
+    return MyTalk.WList.find();
+  }
+});*/

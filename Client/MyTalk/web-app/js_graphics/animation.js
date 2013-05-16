8d7fc4d7bec399c3beef4fe7514af3ee5294aa10
Mattia Agostinetto <agomat@gmail.com>
2013-05-16 21:18:43 +0200

    Modificato file animation.js per uso futuro, quando capiremo come funziona il collegamento View.extend <-> template

--- Client/MyTalk/web-app/js_graphics/animation.js
+++ Client/MyTalk/web-app/js_graphics/animation.js
@@ -26,3 +26,46 @@ $(document).ready(function() {
 
 });
 
+
+
+
+
+
+
+
+$.fn.dropDownMenu = function() { 
+  $(document).click(function() {
+    $("#menu_area").slideUp(400);
+  });
+
+  $('#menu_area').click(function(e) {
+    e.stopPropagation();
+  });
+
+  $('#menu_label').click(function(e) {
+    e.stopPropagation();
+    if ($('#menu_area').css('display') == 'block')
+     $('#menu_area').slideUp(400);
+    else
+     $('#menu_area').slideDown(400);
+  });
+
+  $("#video_tutorial").click(function(){
+    $(this).css('cursor', 'default');
+    $(this).css('opacity', 1);
+    $(this).css('border', 'none');
+    $(this).attr('controls', '');
+    this.play();  
+  });
+}
+
+$.fn.videoTutorial = function() { 
+  $("#video_tutorial").click(function(){
+    $(this).css('cursor', 'default');
+    $(this).css('opacity', 1);
+    $(this).css('border', 'none');
+    $(this).attr('controls', '');
+    this.play();  
+  });
+}
+

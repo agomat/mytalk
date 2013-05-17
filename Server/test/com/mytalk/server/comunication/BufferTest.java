ddba240c73f30ad5f58eae52d47a422e786f4eab
Caprio Armando <armando.caprio@gmail.com>
2013-05-17 15:37:24 +0200

    Classe di test per thread

---
+++ Server/test/com/mytalk/server/comunication/BufferTest.java
@@ -0,0 +1,49 @@
+/**
+* Filename: BufferTest.java
+* Package: com.mytalk.server.comunication
+* Author: 
+* Date:
+*
+* Diary:
+* Version | Date       | Developer | Changes
+* --------+------------+-----------+------------------
+* 0.1	  |	           |           | [+] Inserimento classe, oggetti e costruttore     
+*
+* This software is distributed under GNU/GPL 2.0.
+*
+* Software licensed to:
+* - Zucchetti SRL
+*/
+
+
+package com.mytalk.server.comunication;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.util.Vector;
+
+public class BufferTest {
+		
+	private class PushThread extends Thread{
+		private Vector<Message> toPush= new Vector<Message>();
+		
+		public void addMessage(Message m){
+			toPush.add(m);
+		}
+		
+		public void run(){
+			for(int i=0; i < toPush.size(); i++){
+				BufferIncoming.getInstance().push(toPush.get(i));
+			}
+		}
+		
+	}
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

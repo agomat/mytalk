48e0fd15e8d9446f61ee95203aff56b34ae57ea0
Mattia Agostinetto <agomat@gmail.com>
2013-05-21 17:28:49 +0200

    Convertite le indentazioni in spazi nel file /sql/mytalk.sql in modo che possiamo importarli correttamente da linea di comando nel DBMS MySQL

--- Server/sql/mytalk.sql
+++ Server/sql/mytalk.sql
@@ -16,8 +16,8 @@ CREATE TABLE IF NOT EXISTS Users
 
 CREATE TABLE IF NOT EXISTS Blacklists
 (
-        owner		VARCHAR(20)	NOT NULL,
-        username	VARCHAR(50)	NOT NULL,
+        owner   VARCHAR(20) NOT NULL,
+        username  VARCHAR(50) NOT NULL,
         PRIMARY KEY (owner,username),
         FOREIGN KEY (username) REFERENCES Users(username) ON DELETE CASCADE,
         FOREIGN KEY (owner) REFERENCES Users(username) ON DELETE CASCADE
@@ -25,13 +25,13 @@ CREATE TABLE IF NOT EXISTS Blacklists
 
 CREATE TABLE IF NOT EXISTS Calls
 (
-        id			INTEGER(12)	NOT NULL	AUTO_INCREMENT,
-        caller		VARCHAR(20)	,
-        receiver	VARCHAR(20)	,
-        duration	INTEGER(10)	NOT NULL,
-        startdate	VARCHAR(20) 	NOT NULL,
-        byteSent	INTEGER(10)	NOT NULL,
-        byteReceived	INTEGER(10)	NOT NULL,
+        id      INTEGER(12) NOT NULL  AUTO_INCREMENT,
+        caller    VARCHAR(20) ,
+        receiver  VARCHAR(20) ,
+        duration  INTEGER(10) NOT NULL,
+        startdate VARCHAR(20)   NOT NULL,
+        byteSent  INTEGER(10) NOT NULL,
+        byteReceived  INTEGER(10) NOT NULL,
         PRIMARY KEY (id),
         FOREIGN KEY (caller) REFERENCES Users(username) ON DELETE SET NULL,
         FOREIGN KEY (receiver) REFERENCES Users(username) ON DELETE SET NULL
@@ -39,17 +39,17 @@ CREATE TABLE IF NOT EXISTS Calls
 
 CREATE TABLE IF NOT EXISTS ListNames
 (
-	id			INTEGER(9)	NOT NULL	AUTO_INCREMENT,
-    name		VARCHAR(20)	NOT NULL,
-    owner		VARCHAR(20)	NOT NULL,
+  id      INTEGER(9)  NOT NULL  AUTO_INCREMENT,
+    name    VARCHAR(20) NOT NULL,
+    owner   VARCHAR(20) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (owner) REFERENCES Users(username) ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS UserLists
 (
-        idList		INTEGER(9)	NOT NULL,
-        username	VARCHAR(20)	NOT NULL,
+        idList    INTEGER(9)  NOT NULL,
+        username  VARCHAR(20) NOT NULL,
         PRIMARY KEY (idList,username),
         FOREIGN KEY (idList) REFERENCES ListNames(id) ON DELETE CASCADE,
         FOREIGN KEY (username) REFERENCES Users(username) ON DELETE CASCADE
@@ -57,12 +57,10 @@ CREATE TABLE IF NOT EXISTS UserLists
 
 CREATE TABLE IF NOT EXISTS OnlineUsers
  (
-	ip	 		varchar(40)  NOT NULL,
-	username	 varchar(20),
+  ip      varchar(40)  NOT NULL,
+  username   varchar(20),
   PRIMARY KEY (ip),
   FOREIGN KEY (username) REFERENCES Users(username) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
 ALTER TABLE ListNames AUTO_INCREMENT = 2;
-
-

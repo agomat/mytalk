540059d3f4b5595ee18f2eeb94e82f393e2b6971
Mattia Agostinetto <agomat@gmail.com>
2013-05-23 11:20:46 +0200

    Brevissimo fixamento feature after master

--- Client/com/mytalk/model/modelstruct/DryHelper.js
+++ Client/com/mytalk/model/modelstruct/DryHelper.js
@@ -4,6 +4,10 @@ MyTalk.DryHelper = Ember.Mixin.create({
     return this.get('name') + " " + this.get('surname');
   }.property('name','surname'),
 
+  fullNameConc: function() {
+    return this.get('name').toLowerCase() + this.get('surname').toLowerCase() + "_" + this.get('id');
+  }.property('name','surname'),
+
   avatarTiny: function() {
     return 'http://www.gravatar.com/avatar/' + this.get('md5') + '?s=48&d=blank';
   }.property('md5'),

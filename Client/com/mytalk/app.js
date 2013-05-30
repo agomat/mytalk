4e3c64360aba19be8c345a9efba1c6fca719c9de
Mattia Agostinetto <agomat@gmail.com>
2013-05-30 14:04:11 +0200

    Altro commit pre merge

--- Client/com/mytalk/app.js
+++ Client/com/mytalk/app.js
@@ -13,18 +13,6 @@ Ember.run.later(this, function(){
     $("input[name=username]").change();
     $("input[name=password]").val("user0user0");
     $("input[name=password]").change();
-/*
-    DS.get("defaultStore").adapter.serializer.keyForHasMany = function(type, name) {
-return "list";
-    var key = this.keyForAttributeName(type, name);
-
-    if (this.embeddedType(type, name)) {
-      return key;
-    }
-
-    return this.singularize(key) + "_ids";
-  }
-  */
 }, 3000);
 
 /*

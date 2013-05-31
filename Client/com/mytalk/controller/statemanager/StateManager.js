67b0e2b63982c6f7c9ec327e19373b2c21bcbd7e
Mattia Agostinetto <agomat@gmail.com>
2013-05-31 14:57:36 +0200

    Statemanager aggiornato

--- Client/com/mytalk/controller/statemanager/StateManager.js
+++ Client/com/mytalk/controller/statemanager/StateManager.js
@@ -1,63 +1,85 @@
 MyTalk.StateManager = Ember.StateManager.create({
+  
+  enableLogging: true,
+
   initialState: "isNotAuthenticated",
+
   isAuthenticated: Ember.State.create({
     enter: function () {
-      console.log("[StateManager] enter " + this.name);
+     
     },
     logout: function (manager, context) {
       manager.transitionTo('isNotAuthenticated');
     }
   }),
+
   isNotAuthenticated: Ember.State.create({
     enter: function () {
-      console.log("[StateManager] enter " + this.name);
+    
     },
-    login: function (manager) {
+    login: function (manager, context) {
       manager.transitionTo('isAuthenticated');
       MyTalk.Router.router.transitionTo('logged.index');
     }
   }),
+
   isBusy: Ember.State.create({
+
+    campoDatoStrutturato: null;
+        /* 
+            statistics -> {duration: int sentBytes: int ,receivedBytes: int}
+            RTCinfo ->  {
+                          myIP: ('string'),
+                          myUserId: ('string'),
+                          myRTCinfo: ('JSON di candidati e SDP'),
+                          speakerIp: ('string'),
+                          speakerUserId: ('string'),
+                          speakerRTCinfo: ('JSON di candidati e SDP')
+                        }
+        */
+
     enter: function () {
-      console.log("[StateManager] enter " + this.name);
+     
     },
     free: function (manager, context) {
       manager.transitionTo('isNotBusy');
     },
     
-    isCall: Ember.State.create({
+    outcomingCall: Ember.State.create({
       enter: function () {
-        console.log("[StateManager] enter " + this.name);
-        },
-      free: function (manager, context) {
-        manager.transitionTo('isNotBusy');
-      }
-    }),
-    
-    isConnected: Ember.State.create({
-      enter: function () {
-        console.log("[StateManager] enter " + this.name);
+        
       },
       free: function (manager, context) {
         manager.transitionTo('isNotBusy');
       }
     }),
-    
-    waitingConnection: Ember.State.create({
+
+    incomingCall: Ember.State.create({
       enter: function () {
-        console.log("[StateManager] enter " + this.name);
+        
+      },
+      free: function (manager, context) {
+        manager.transitionTo('isNotBusy');
+      }
+    }),
+
+    isConnected: Ember.State.create({
+      enter: function () {
+        
       },
       free: function (manager, context) {
         manager.transitionTo('isNotBusy');
       }
     }),
   }),
+
   isNotBusy: Ember.State.create({
     enter: function () {
-      console.log("[StateManager] enter " + this.name);
+      
     },
     busy: function (manager, credentials) {
       manager.transitionTo('isBusy');
     }
   })
+
 });

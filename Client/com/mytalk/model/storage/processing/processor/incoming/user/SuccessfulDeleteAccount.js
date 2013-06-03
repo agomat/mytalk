9216bdf3ded9fefe80bf5b1c385e94b746743965
Mattia Agostinetto <agomat@gmail.com>
2013-06-03 19:14:28 +0200

    Aggiunte altre richieste

---
+++ Client/com/mytalk/model/storage/processing/processor/incoming/user/SuccessfulDeleteAccount.js
@@ -0,0 +1,30 @@
+/**
+* Filename: SuccessfulDeleteAccount.js
+* Package: com.mytalk.storage.processing.processor.incoming.user
+* Dependencies: com.mytalk.storage.processing.processor.incoming.AbstractInProcessorProduct
+* Author: Mattia Agostinetto
+* Date: 2013-05-01
+*
+* Diary:
+*
+| Version | Date       | Developer | Changes
+* --------+------------+-----------+------------------
+* 0.1     | 2013-05-01 | MA        | [+] Stesura dell'intera classe
+*
+* This software is distributed under GNU/GPL 2.0.
+*
+* Software licensed to:
+* - Zucchetti SRL
+*/
+
+MyTalk.processor.SuccessfulDeleteAccount = Ember.Object.extend(MyTalk.AbstractInProcessorProduct, {
+  name: 'SuccessfulDeleteAccount',
+
+  process: function (ari) {
+    console.error("Processor "+this.get('name')+" non esistente TODO");
+  },
+  
+  getProcessorName: function () {
+    return this.get('name');
+  }
+});

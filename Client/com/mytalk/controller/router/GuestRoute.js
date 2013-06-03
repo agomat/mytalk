b7b080e2ce14dec9a7e9818ea714523f21841e75
Massimo Griggio <great_max@hotmail.it>
2013-06-03 20:21:37 +0200

    corretto route

--- Client/com/mytalk/controller/router/GuestRoute.js
+++ Client/com/mytalk/controller/router/GuestRoute.js
@@ -1,9 +1,6 @@
 MyTalk.GuestRoute = Ember.Route.extend({
   renderTemplate: function() {
     this.render('guest', { into: 'index', outlet: 'content' });
-  },
-  setupController: function() {
-    this.controllerFor('ipcall').set('model', MyTalk.PersonalData.find(0));
   }
 //   model: function() {
 //     /*

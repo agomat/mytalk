e21dc3d27527334c63b84725bd24e4ade5ea5aac
Stefano Campese <stefano.campese.90@gmail.com>
2013-06-05 10:22:43 +0200

    aggiunta dell'header nell'app.js

--- Client/com/mytalk/client/app.js
+++ Client/com/mytalk/client/app.js
@@ -1,3 +1,22 @@
+/**
+* Filename: app.js
+* Package: com.mytalk.client
+* Dependencies:
+* Author: Griggio Massimo
+* Date: 2013-04-26
+*
+* Diary:
+*
+| Version | Date       | Developer | Changes
+* --------+------------+-----------+------------------
+* 0.1     | 2013-04-05 | MG        | [+] Scrittura classe
+*
+* This software is distributed under GNU/GPL 2.0.
+*
+* Software licensed to:
+* - Zucchetti SRL
+*/
+
 MyTalk = Ember.Application.create({
   LOG_TRANSITIONS: true
 });

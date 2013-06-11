bcaf0c5646048af53c79c037f3a6cc1fc5f16304
Stefano Campese <stefano.campese.90@gmail.com>
2013-06-11 10:45:14 +0200

    sistemato commento su ipcallController (per la gioia di mattia) e commentata la classe ARI

--- Client/com/mytalk/client/model/storage/ARI.js
+++ Client/com/mytalk/client/model/storage/ARI.js
@@ -15,12 +15,51 @@
 *
 * Software licensed to:
 * - Zucchetti SRL
+*
+* Description: Definisce la struttura del pacchetto di comunicazione scambiato con il server.
+*
 */
 
 MyTalk.ARI = Ember.Object.extend({
+  
+  /**
+   * Proprietà che contiene username e password dell'utente
+   * @property -auth           
+   * @type {String}                   
+   *
+  */
+
   auth: undefined,
+
+  /**
+   * Proprietà che contiene la richiesta da eseguire.
+   * @property -req           
+   * @type {String}                   
+   *
+  */
+
   req: undefined,
+
+  /**
+   * Proprietà che contiene le informazioni da scambiare col server necessarie per soddisfare la richiesta.
+   * @property -info           
+   * @type {String}                   
+   *
+  */
+
   info: undefined,
+
+  /**
+   * Questo metodo si attiva ogni qual volta viene creata un'istanza della classe $ARI$, tale metodo si occupa di settare i campi dati della classe.
+   * I campi dati $auth$ e $req$ vengono settati rispettivamente con i parametri $a$ e $r$ passati al metodo, mentre per settare il campo dati $info$ viene applicata la funzione javacript 
+   * _JSON.stringify()_  al parametro $i$ passato al metodo; tale funzione è necessaria al fine di convertire il JSON, ( contente le informazioni scambiate con il server ), in stringa.
+   *
+   * @method -init
+   * @param {String} $a$ è la stringa da settare nel campo dati $auth$       
+   * @param {String} $r$ è la stringa da settare nel campo dati $req$
+   * @param {Object} $i$ è l'oggetto JSON contenente le informazioni.                                 
+   * @return {Void} 
+  */
   
   init: function(a, r, i) {
     this.set('auth', a);
@@ -28,27 +67,73 @@ MyTalk.ARI = Ember.Object.extend({
     this.set('info', JSON.stringify(i));
     this._super();
   },
+  
+  /**
+   * Questo metodo si occupa ritornare il valore del campo dati $auth$ dell'istanza della classe in questione
+   *
+   * @method +getAuth                                 
+   * @return {String} 
+  */
 
   getAuth: function() {
     return this.get('auth');
   },
   
+  /**
+   * Questo metodo si occupa ritornare il valore del campo dati $req$ dell'istanza della classe in questione
+   *
+   * @method +getReq                                 
+   * @return {String} 
+  */
+
   getReq: function() {
     return this.get('req');
   },
   
+  /**
+   * Questo metodo si occupa ritornare il valore del campo dati $info$ dell'istanza della classe in questione
+   *
+   * @method +getInfo                                 
+   * @return {String} 
+  */
+
   getInfo: function() {
     return this.get('info');
   },
+
+  /**
+   * Questo metodo si occupa settare il valore del campo dati $auth$ dell'istanza della classe in questione
+   *
+   * @method -setAuth
+   * @param {String} $a$ è la stringa da settare nel campo dati $auth$                                 
+   * @return {Void} 
+  */
 
   setAuth: function(a) {
     this.set('auth', a);
   },
   
+  /**
+   * Questo metodo si occupa settare il valore del campo dati $req$ dell'istanza della classe in questione
+   *
+   * @method -setReq
+   * @param {String} $r$ è la stringa da settare nel campo dati $req$                                 
+   * @return {Void} 
+  */
+
   setReq: function(r) {
     this.set('req', r);
   },
   
+  /**
+   * Questo metodo si occupa settare il valore del campo dati $info$ dell'istanza della classe in questione.
+   * Per settare tale campo dati il metodo applica al parametro $i$ la funzione javascript _JSON.stringify()_ al fine di convertire l'oggetto $JSON$ scambiato col server in una stringa.
+   *
+   * @method -setInfo
+   * @param {Object} $i$ è l'oggetto JSON contenente le informazioni.                                  
+   * @return {Void} 
+  */
+
   setInfo: function(i) {
     this.set('info', JSON.stringify(i));
   }

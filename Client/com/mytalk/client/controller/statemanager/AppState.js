308c295539b406de479c25627faa60c327a061fb
Mattia Agostinetto <agomat@gmail.com>
2013-07-13 15:27:19 +0200

    Rimosse dipendenze in AppState e CallState

--- Client/com/mytalk/client/controller/statemanager/AppState.js
+++ Client/com/mytalk/client/controller/statemanager/AppState.js
@@ -1,7 +1,7 @@
 /**
 * Filename: AppState.js
 * Package: com.mytalk.client.controller.statemanager
-* Dependencies: com.mytalk.client.controller.router.Router
+* Dependencies:
 * Author: Agostinetto Mattia
 * Date: 2013-04-23
 *
--- Client/com/mytalk/client/controller/statemanager/CallState.js
+++ Client/com/mytalk/client/controller/statemanager/CallState.js
@@ -1,7 +1,7 @@
 /**
 * Filename: CallState.js
 * Package: com.mytalk.client.controller.statemanager
-* Dependencies: com.mytalk.client.controller.router.Router
+* Dependencies:
 * Author: Agostinetto Mattia
 * Date: 2013-04-23
 *
